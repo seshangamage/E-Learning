30c7b0e23dc73ff30f9fccc5505d267e0037fcf1
Sheshan <seshangamage@gmail.com>
2024-10-24 21:26:57 +0530

    Spring Bear Bone Project

---
+++ Spring-Boot-Course/laptopstore/src/test/java/com/sheshan/laptopstore/LaptopstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sheshan.laptopstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaptopstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
