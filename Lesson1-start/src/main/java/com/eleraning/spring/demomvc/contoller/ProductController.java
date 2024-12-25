2ee5d43d392af4e3555a73dda4397f17e3825821
Sheshan <seshangamage@gmail.com>
2024-12-25 21:26:49 +0530

    Error Handling in form Submission

--- Lesson1-start/src/main/java/com/eleraning/spring/demomvc/contoller/ProductController.java
+++ Lesson1-start/src/main/java/com/eleraning/spring/demomvc/contoller/ProductController.java
@@ -5,11 +5,21 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.eleraning.spring.demomvc.model.Product;
+import com.eleraning.spring.demomvc.model.ProductDto;
 import com.eleraning.spring.demomvc.services.ProductRepository;
+
+import jakarta.validation.Valid;
+
+import org.springframework.web.bind.annotation.PostMapping;
+
+
 
 @Controller
 @RequestMapping("/products")
@@ -25,5 +35,24 @@ public class ProductController {
         return "products/products";
 
     }
+
+    @GetMapping("/create")
+    public String getCreateProdcutPage(Model model) {
+        ProductDto productDto = new ProductDto();
+        model.addAttribute("productDto", productDto);
+        return "products/createProdcut";
+    }
+
+    @PostMapping("/create")
+    public String createProdcut(@Valid @ModelAttribute ProductDto productDto,BindingResult result) {
+        if(productDto.getImageFile() == null || productDto.getImageFile().isEmpty()){
+            result.addError(new FieldError("prodcutDto", "imageFile", "The Image file isn empty"));
+        }
+        if(result.hasErrors()){
+            return "products/createProdcut";
+        }
+        return "redirect:/products";
+    }
+    
     
 }
