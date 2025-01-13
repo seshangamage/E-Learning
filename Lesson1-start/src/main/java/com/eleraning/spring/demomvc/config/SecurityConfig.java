c31be3cc098804145cc9e439ae49950cc0cb6f07
Sheshan <seshangamage@gmail.com>
2025-01-13 18:57:39 +0530

    Login Required Logic added

---
+++ Lesson1-start/src/main/java/com/eleraning/spring/demomvc/config/SecurityConfig.java
@@ -0,0 +1,28 @@
+package com.eleraning.spring.demomvc.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http.authorizeHttpRequests(auth -> auth
+                    .requestMatchers("/").permitAll()
+                    .requestMatchers("/images/*").permitAll()
+                    .anyRequest().authenticated()
+        )
+        .formLogin(form -> form
+                .defaultSuccessUrl("/", true)
+        )
+        .logout(config -> config.logoutSuccessUrl("/"))
+        .build();
+    }
+    
+}
